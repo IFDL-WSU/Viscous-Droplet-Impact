70cc63df36f7c3921a6cc214741a4db6382d36c6
QuantumKraken <33362023+QuantumKraken@users.noreply.github.com>
2020-10-07 15:40:05 -0700

    Change frame2file to process 1 frame at a time.

--- Image-Processing/frame2file.m
+++ Image-Processing/frame2file.m
@@ -1,4 +1,4 @@
-function frame2file(M,name,path,ext)
+function frame2file(M,name,path,ext,frame)
 % FRAME2FILE Converts a matrix of frames M into a series of pictures of an file type.
 %    FRAME2FILE(M,filename, path, ext) converts a frames matrix M to files of the format
 %    'ext' with the numbered name 'name' to the folder specified with 'path'. 
@@ -9,7 +9,5 @@ function frame2file(M,name,path,ext)
 
     [~,~,~,fRange] = size(M); %Number of frames.
     % Convert each matrix entry to to an image and export.
-    for i = 1:fRange
-          imwrite(M(:,:,:,i),append(path,"/", name, string(i), ".", ext));
-    end
+          imwrite(M(:,:,:,frame),append(path,"/", name, string(i), ".", ext));
 end
