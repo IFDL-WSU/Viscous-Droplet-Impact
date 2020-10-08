c2daf33d3f5067fe36717de5f4ed3978f301908c
QuantumKraken <33362023+QuantumKraken@users.noreply.github.com>
2020-10-07 18:38:45 -0700

    Update frame2file.m

--- Image-Processing/frame2file.m
+++ Image-Processing/frame2file.m
@@ -1,5 +1,5 @@
 function frame2file(M,name,path,ext,frame)
-% FRAME2FILE Converts a matrix of frames M into a series of pictures of an file type.
+% FRAME2FILE Converts a frame from a matrix of frames M into an image file.
 %    FRAME2FILE(M,filename, path, ext, frame) converts the selected frame of number "frame" 
 %    in matrix M to a file at directory "path" of the format specified by "ext". 
 %
