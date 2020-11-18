53bd7b9381d8bcba87b6cfa5c092f9645ce074c3
Rosemary Barrass <55859958+RosemaryBarrass@users.noreply.github.com>
2020-11-17 19:32:43 -0800

    Fix bugs surrounding uigetfile
    This commit fixes the bug that caused uigetfile to run when the background was pressed. This commit also fixes the error message that appears when a user presses cancel after uigetfile is called. This commit fixes the bug that causes the applet to go to the background when uigetfile is called.

---
+++ GUI/maxSpread.m
@@ -0,0 +1,18 @@
+function max_spread = maxSpread(dat)
+  % This function finds the max spread radius (in pixels)
+  %
+  % d -          Number of frames in the data set.
+  % spread -     dx1 matrix of zeros that is filled in with the radius of the droplet in each frame.
+  % max_spread - This is the maximum radius (in pixels) of the spread matrix.
+  
+  [~,~,~,d] = size(dat); 
+  
+  spread = zeros(d,1);
+  
+  for i = 1:d
+      m = regionprops(dat(:,:,:,i), 'Extrema');
+      spread(i) = (m(1,1).Extrema(5,1) - m(1,1).Extrema(6,1))/2; 
+  end
+  
+  max_spread = max(spread);
+end
