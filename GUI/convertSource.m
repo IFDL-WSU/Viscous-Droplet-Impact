e1af078b52145526e77c8a8d04b28eeca9b83652
QuantumKraken <33362023+QuantumKraken@users.noreply.github.com>
2021-03-03 10:01:48 -0800

    Update convertSource.m
    Fixed bug that turns gray-scale image into black&white.

--- GUI/convertSource.m
+++ GUI/convertSource.m
@@ -1,21 +1,21 @@
 function converted_Source_Collection = convertSource(image_Collection, floor_Angle)
 %% Convert source image to TrueColor image. %%
 [a,b,c,d]=size(image_Collection);
-converted_Source_Collection = zeros(a,b,3,d);
+converted_Source_Collection = uint8(zeros(a,b,3,d));
 
 if ndims(image_Collection) < 4
     error('Unexpected Source Matrix format. Please provide a grayscale image, an RGB image, or a binary image. ')
 elseif islogical(image_Collection) == 1
     for n = 1:3
-        converted_Source_Collection(:,:,n,:) = uint8(image_Collection(:,:,1,:));
+        converted_Source_Collection(:,:,n,:) = im2uint8(image_Collection(:,:,1,:));
     end
 elseif c == 1
     for n = 1:3
-        converted_Source_Collection(:,:,n,:) = uint8(image_Collection(:,:,1,:));
+        converted_Source_Collection(:,:,n,:) = im2uint8(image_Collection(:,:,1,:));
     end
 elseif (c == 3)
-    if isa(sourceM,'uint8') == 0
-        converted_Source_Collection(:,:,:,:) = uint8(image_Collection(:,:,:,:));
+    if isa(image_Collection,'uint8') == 0
+        converted_Source_Collection(:,:,:,:) = im2uint8(image_Collection(:,:,:,:));
     end
 else
     error('Unexpected Source Matrix format. Please provide a grayscale image, an RGB image, or a binary image. ')
