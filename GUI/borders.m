66d2a8b378f64fb2c680d5a1358d0db4536deeb0
FunkyMonkeyProgrammer <71910798+FunkyMonkeyProgrammer@users.noreply.github.com>
2021-04-07 00:21:53 -0700

    Update borders.m

--- GUI/borders.m
+++ GUI/borders.m
@@ -1,18 +1,18 @@
-function videoBorders = borders(dat)
+function videoBorders = borders(videoSource)
 % extract dimensions of video matrix
-[a, b, c, d] = size(dat);
+videoSize = size(videoSource);
 
-for i = 1:d
+for i = 1:videoSize(4)
     % Selects each frame and uploads it as a single double image.
     % *32 multiplier brightens the image to reduce inconsistancies. 
-    I = im2double(dat(:,:,1,i))*32;
+    frame = im2double(videoSource(:,:,:,i))*32;
     % Convert back to uint8
-    I = im2uint8(I);
+    frame = im2uint8(frame);
     
     % Use the processing techniques from the exercise we did the other day.
-    [~, threshold] = edge(I, 'sobel');
+    [~, threshold] = edge(frame, 'sobel');
     fudgeFactor = 0.25;
-    BWs = edge(I, 'prewitt', threshold * fudgeFactor);
+    BWs = edge(frame, 'prewitt', threshold * fudgeFactor);
     
     se90 = strel('line', 3, 90);
     se0 = strel('line', 3, 0);
